5afd173083d71fe12d2a92637835a425a81090c6
Lincon Martins <linconmartins18@gmail.com>
2022-09-09 12:47:52 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import NotificatioButton from "./components/NotificationButton"
+import Header from "./components/Header"
+
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificatioButton />
-      <NotificatioButton />
-      <NotificatioButton />
-      <NotificatioButton />
-      <NotificatioButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
